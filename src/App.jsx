9c73ef17f5b66abfbb1882be32910b8a6ba6a097
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2024-08-17 12:46:16 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,34 @@ function App() {
 }
 
 export default App
+import user from './data/user.json';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import data from './data/data.json';
+import FriendList from './FriendList/FriendList';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import SectionText from './SectionText/SectionText';
+export const App = () => {
+  return (
+    <>
+      <SectionText text="1 - Профіль соціальної мережі" />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <SectionText text="2- Секція статистики" />
+      <Statistics title="Upload stats" stats={data} />
+
+      <SectionText text="3 - Список друзів" />
+      <FriendList friends={friends} />
+
+      <SectionText text="4 - Історія транзакцій" />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
